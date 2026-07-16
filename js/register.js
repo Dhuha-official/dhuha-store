0a30ddc16e129a183e5628a6054187624d35f500
Dhuha-official <gecrave.id@gmail.com>
2026-07-16 20:14:30 +0700

    Create register.js

---
+++ js/register.js
@@ -0,0 +1,37 @@
+document.getElementById("register-form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirm-password").value;
+
+    if (password !== confirmPassword) {
+        alert("Password dan konfirmasi password tidak sama.");
+        return;
+    }
+
+    try {
+
+        const { data, error } = await window.supabaseClient.auth.signUp({
+    email,
+    password,
+    options: {
+        data: {
+            name: name
+        }
+    }
+});
+
+        if (error) {
+            alert(error.message);
+            return;
+        }
+
+        alert("Pendaftaran berhasil!");
+        window.location.href = "login.html";
+
+    } catch (err) {
+        alert("Catch Error: " + err.message);
+    }
+});
