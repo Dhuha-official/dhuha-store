7f282c20d2ac25bdcf836ba1dfd59d233946ba32
Dhuha-official <gecrave.id@gmail.com>
2026-07-16 15:17:21 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -5,36 +5,3 @@ window.supabaseClient = window.supabase.createClient(
     supabaseUrl,
     supabaseKey
 );
-
-async function loadProducts() {
-    const container = document.getElementById("product-list");
-    if (!container) return;
-
-    const { data, error } = await window.supabaseClient
-        .from("products")
-        .select("*");
-
-    if (error) {
-        console.error(error);
-        return;
-    }
-
-    container.innerHTML = "";
-
-    data.forEach(product => {
-        container.innerHTML += `
-<div class="product">
-    <img src="${product.image_url}" alt="${product.name}" class="product-image">
-
-    <h3>${product.name}</h3>
-
-    <p>Rp ${Number(product.price).toLocaleString("id-ID")}</p>
-
-    <a href="product.html?id=${product.id}" class="btn-product">
-        Lihat Produk
-    </a>
-</div>`;
-    });
-}
-
-loadProducts();
