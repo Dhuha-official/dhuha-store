0cf6884276936b240f70c315007cbb99c059f4f1
Dhuha-official <gecrave.id@gmail.com>
2026-07-18 00:39:44 +0700

    Delete supabase.js

--- supabase.js
+++
@@ -1,7 +0,0 @@
-const supabaseUrl = "https://valwpmiwzaqgijtrbizl.supabase.co";
-const supabaseKey = "sb_publishable_REbhQNOsjfE4aT5oMXX6YA_COOzqYF8";
-
-window.supabaseClient = window.supabase.createClient(
-    supabaseUrl,
-    supabaseKey
-);
