0dae1e088dada8d74c199bbc8474fa46f3a61efe
Dhuha-official <gecrave.id@gmail.com>
2026-07-18 15:31:25 +0700

    Update product.js

--- js/product.js
+++ js/product.js
@@ -46,5 +46,39 @@ async function loadProduct(){
 }
 
 function addToCart(){
+loadProduct();
 
+const btn = document.getElementById("add-cart");
+
+if(btn){
+    btn.onclick = addToCart;
+}
+
+const wishBtn = document.getElementById("wishlist-btn");
+
+if(wishBtn){
+
+    wishBtn.onclick = () => {
+
+        let wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
+
+        const exist = wishlist.find(item => item.id === productData.id);
+
+        if(!exist){
+
+            wishlist.push(productData);
+
+            localStorage.setItem("wishlist", JSON.stringify(wishlist));
+
+            alert("Ditambahkan ke Wishlist");
+
+        }else{
+
+            alert("Produk sudah ada di Wishlist");
+
+        }
+
+    };
+
+}
     let
