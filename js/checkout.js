2cd6e1ae3971038d663acbf12f95f48b88ea48e2
Dhuha-official <gecrave.id@gmail.com>
2026-07-20 00:53:40 +0700

    Implement subtotal calculation and shipping cost update
    Calculate subtotal and update shipping costs based on selected courier.

--- js/checkout.js
+++ js/checkout.js
@@ -112,3 +112,65 @@ addressResult.style.display="none";
 }
 
 });
+let subtotal = 0;
+
+const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+cart.forEach(item=>{
+
+subtotal += item.price * item.qty;
+
+});
+
+document.getElementById("subtotal").innerText =
+"Rp " + subtotal.toLocaleString("id-ID");
+const shippingRadio =
+document.querySelectorAll("input[name='shipping']");
+
+shippingRadio.forEach(radio=>{
+
+radio.addEventListener("change",updateShipping);
+
+});
+
+updateShipping();
+
+function updateShipping(){
+
+let cost = 0;
+
+const courier =
+document.querySelector("input[name='shipping']:checked").value;
+
+switch(courier){
+
+case "JNE":
+
+cost = 18000;
+
+break;
+
+case "J&T":
+
+cost = 20000;
+
+break;
+
+case "SiCepat":
+
+cost = 17000;
+
+break;
+
+}
+
+document.getElementById("shippingCost").innerText =
+"Rp " + cost.toLocaleString("id-ID");
+
+document.getElementById("shippingTotal").innerText =
+"Rp " + cost.toLocaleString("id-ID");
+
+document.getElementById("checkout-total").innerText =
+"Rp " + (subtotal + cost).toLocaleString("id-ID");
+
+    }
