faaa40986f0d519babe0f50e6d469bcba595d999
Dhuha-official <gecrave.id@gmail.com>
2026-07-21 22:21:53 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,8 @@
-const SUPABASE_URL = "ISI_URL_SUPABASE_KAMU";
+const SUPABASE_URL = "https://valwpmiwzaqgijtrbizl.supabase.co";
 
-const SUPABASE_ANON_KEY = "ISI_ANON_KEY_KAMU";
+const SUPABASE_KEY = "PASTE_PUBLISHABLE_KEY_KAMU_DI_SINI";
 
 const supabase = window.supabase.createClient(
     SUPABASE_URL,
-    SUPABASE_ANON_KEY
+    SUPABASE_KEY
 );
