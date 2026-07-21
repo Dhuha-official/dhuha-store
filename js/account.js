78ab223fa0866e6c644c01680286220440d0ceea
Dhuha-official <gecrave.id@gmail.com>
2026-07-21 21:43:28 +0700

    Update account.js

--- js/account.js
+++ js/account.js
@@ -1,9 +1,49 @@
+// ======================================
+// DHUHA ACCOUNT
+// ======================================
+
 const user = JSON.parse(localStorage.getItem("user"));
 
-if(user){
+const name = document.getElementById("user-name");
+const email = document.getElementById("user-email");
 
-document.getElementById("user-name").innerText = user.name;
+const loginArea = document.getElementById("login-area");
+const logoutArea = document.getElementById("logout-area");
 
-document.getElementById("user-email").innerText = user.email;
+if (user) {
+
+    if (name) {
+        name.innerText = user.name;
+    }
+
+    if (email) {
+        email.innerText = user.email;
+    }
+
+    if (loginArea) {
+        loginArea.style.display = "none";
+    }
+
+    if (logoutArea) {
+        logoutArea.style.display = "block";
+    }
+
+} else {
+
+    if (name) {
+        name.innerText = "Guest";
+    }
+
+    if (email) {
+        email.innerText = "Silakan login untuk melanjutkan";
+    }
+
+    if (loginArea) {
+        loginArea.style.display = "block";
+    }
+
+    if (logoutArea) {
+        logoutArea.style.display = "none";
+    }
 
 }
