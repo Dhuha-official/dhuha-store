265d5b18c4d46b66b4cf1f815ce3d709db6baf59
Dhuha-official <gecrave.id@gmail.com>
2026-07-22 03:42:48 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://valwpmiwzaqgijtrbizl.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://valwpmiwzaqgijtrbizl.supabase.co";
 const SUPABASE_KEY = "sb_publishable_REbhQNOsjfE4aT5oMXX6YA_COOzqYF8";
 
 const { createClient } = window.supabase;
