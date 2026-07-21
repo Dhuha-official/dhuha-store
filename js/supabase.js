f8127009bd0da76dadbe89aa503554c7dbec44ea
Dhuha-official <gecrave.id@gmail.com>
2026-07-22 02:59:21 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -6,10 +6,10 @@ const SUPABASE_URL =
 "https://valwpmiwzaqgijtrbizl.supabase.co";
 
 const SUPABASE_KEY =
-"YOUR_PUBLISHABLE_KEY";
+"sb_publishable_REbhQNOsjfE4aT5oMXX6YA_COOzqYF8";
 
 const supabase =
 window.supabase.createClient(
-SUPABASE_URL,
-SUPABASE_KEY
+    SUPABASE_URL,
+    SUPABASE_KEY
 );
