0656103bdc0f381ce278ecfbbdc1c565de80643c
Dhuha-official <gecrave.id@gmail.com>
2026-07-22 03:24:23 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,16 +1,7 @@
-// ======================================
-// DHUHA SUPABASE
-// ======================================
+const SUPABASE_URL = "https://valwpmiwzaqgijtrbizl.supabase.co";
+const SUPABASE_KEY = "sb_publishable_REbhQNOsjfE4aT5oMXX6YA_COOzqYF8";
 
-const SUPABASE_URL =
-"https://valwpmiwzaqgijtrbizl.supabase.co";
-
-const SUPABASE_KEY =
-"sb_publishable_REbhQNOsjfE4aT5oMXX6YA_COOzqYF8";
-
-const supabase = window.supabase.createClient(
+const supabaseClient = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
-
-window.supabaseClient = supabase;
